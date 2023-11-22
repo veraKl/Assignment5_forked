211bd38c558528278e54056a5709e32774852923
Alex22H <hardea@usf.edu>
2023-11-22 14:00:52 -0500

    added code for pulling in API data

--- MVC_EF_Start/Controllers/HomeController.cs
+++ MVC_EF_Start/Controllers/HomeController.cs
@@ -11,9 +11,66 @@ namespace MVC_EF_Start.Controllers
 {
   public class HomeController : Controller
   {
-    public IActionResult Index()
-    {
-      return View();
+
+        HttpClient? httpClient;
+
+        static string BASE_URL = "https://data.wa.gov/resource/f6w7-q2d2.json";
+        static string API_KEY = "hcEy0sdQxZG53NCsiQQIsBGH8tmwt2o3gbPmmwcG";
+
+        public ApplicationDbContext dbContext;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string WA_EV_API_PATH = BASE_URL;
+            string electricVehicleData = "";
+
+            Vehicles? vehicles = null;
+
+            //httpClient.BaseAddress = new Uri(NATIONAL_PARK_API_PATH);
+            httpClient.BaseAddress = new Uri(WA_EV_API_PATH);
+
+            try
+            {
+                //HttpResponseMessage response = httpClient.GetAsync(NATIONAL_PARK_API_PATH)
+                //                                        .GetAwaiter().GetResult();
+                HttpResponseMessage response = httpClient.GetAsync(WA_EV_API_PATH)
+                                                        .GetAwaiter().GetResult();
+
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    electricVehicleData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!electricVehicleData.Equals(""))
+                {
+                    //JsonConvert is part of the NewtonSoft.Json Nuget package
+                    vehicles = JsonConvert.DeserializeObject<Vehicles>(electricVehicleData);
+                }
+
+                dbContext.Vehicles.Add(vehicles);
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View(vehicles);
+            //return View();
+        }
     }
-  }
 }
